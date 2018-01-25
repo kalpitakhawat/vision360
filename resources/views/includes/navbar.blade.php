1dcc13ce9d79efbe95ccaf123ca2a9e936a58eb3
Jimish Fotariya <fotariyajimish@gmail.com>
2018-01-25 21:35:27 +0530

    navbar update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,14 +1,15 @@
-<header>
-  <nav class="navbar navbar-expand-lg navbar-dark blue-gradient" style="background: linear-gradient(40deg,#fff,#45cafc,#303f9f) !important;">
-                <a class="navbar-brand" href="#">
-                  <img src="/img/vision_360.png" height="70px;" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-3" aria-controls="navbarSupportedContent-3" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent-3">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
+
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark info-color fixed-top top-nav-collapse">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="/img/vision_360.png" height="70px;" /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+                    <ul class="navbar-nav mr-auto">
+                         <li class="nav-item active">
                             <a class="nav-link waves-effect waves-light" href="/">Home <span class="sr-only">(current)</span></a>
                         </li>
                          <li class="nav-item">
@@ -29,6 +30,10 @@
                         <li class="nav-item">
                             <a class="nav-link waves-effect waves-light" href="#">Contact Us</a>
                         </li>
+
+                    </ul>
+
+                    <ul class="navbar-nav ml-auto nav-flex-icons">
                         @if(Auth::check())
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i>
@@ -45,6 +50,6 @@
                         @endif
                     </ul>
                 </div>
-            </nav>
-            
-</header>
+            </div>
+        </nav>
+
