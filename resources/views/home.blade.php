b94ac32506ac971735738e1033c965b80c749be1
kalpitakhawat <kalpitakhawat@gmail.com>
2018-01-25 21:08:34 +0530

    new landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,10 +55,11 @@
         <!--Intro Section-->
         <section class="view intro-2 hm-gradient">
             <div class="full-bg-img">
-                <div class="container flex-center">
+                <div class="container-fluid flex-center">
                     <div class="row flex-center pt-5 mt-3">
-                        <div class="col-md-12 col-lg-6 text-center text-md-left margins">
-                            <div class="white-text">
+                        <div class="col-md-12 col-lg-8 text-center text-md-left margins">
+                            <img src="/img/banner.png" alt="" class="wow fadeInLeft" >
+                            <!-- <div class="white-text">
                                 <h1 class="h1-responsive font-bold wow fadeInLeft" data-wow-delay="0.3s">Make purchases with our app </h1>
                                 <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
                                 <h6 class="wow fadeInLeft" data-wow-delay="0.3s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt
@@ -69,11 +70,11 @@
                                 <a class="btn btn-white btn-rounded blue-text font-bold ml-lg-0 wow fadeInLeft" data-wow-delay="0.3s">Download</a>
                                 <a class="btn btn-outline-white btn-rounded font-bold wow fadeInLeft" data-wow-delay="0.3s">Learn more 
                                     </a>
-                            </div>
+                            </div> -->
                         </div>
 
-                        <div class="col-md-12 col-lg-6  wow fadeInRight d-flex justify-content-center" data-wow-delay="0.3s">
-                            <img src="https://mdbootstrap.com/img/Mockups/Transparent/Small/admin-new.png" alt="" class="img-fluid">
+                        <div class="col-md-12 col-lg-4 wow fadeInRight d-flex justify-content-center" data-wow-delay="0.3s">
+                            <img src="/img/Globe.png" >
                         </div>
                     </div>
                 </div>
