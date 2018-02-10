2d3979740111970cb350551370737e08526b3407
Jimish Fotariya <fotariyajimish@gmail.com>
2018-02-10 12:16:31 +0530

    4 circular users

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -11,16 +11,23 @@ class RootController extends Controller
 {
     public function index(Request $r)
     {
-    		// $startdate= new Carbon(env('START_DATE'));
-    		// $enddate = Carbon::now();
-    		// $difference = $enddate->diff($startdate)->days;
-    		// $u = User::where('status' , 'active')->where('type','user')->get();
-    		// $offset = (($difference * 4) % $u->count());
-    		$user=User::where('status' , 'active')->where('type','user')->skip(0)->take(4)->get();
+    		$startdate= new Carbon("2018-02-01");
+    		$enddate = Carbon::now();
+    		$difference = $enddate->diff($startdate)->days;
+            $userCount = User::where('status' , 'active')->count();
+            $offset = (($difference-1) * 4) % $userCount;
+            $u = User::where('status' , 'active')->where('id','>',$offset)->take(4)->get();
+            
+            if( $u->count() < 4) {
+                $firstXUsers = User::where('status' , 'active')->take( 4 - $u->count()  )->get();    
+                $u = $u->merge($firstXUsers);
+            }
 
     		$circular = Circular::whereDate('created_at', '>=', Carbon::now()->subDays(3))->get();
 
-    		$blogs = Blog::where('status' , 'active')->where('isActive' , 'true')->orderBy('created_at','DESC')->skip(0)->take(3)->get();
+    		$blogs = Blog::where('status' , 'active')
+                            // ->where('isActive' , 'true')
+                            ->orderBy('created_at','DESC')->skip(0)->take(3)->get();
     		foreach ($blogs as $blog) {
     			$bu = User::find($blog->user_id);
     			$blog->user_name = $bu->f_name . " " . $bu->l_name;
@@ -29,7 +36,7 @@ class RootController extends Controller
     		
     		$e = Event::where('isActive','true')->where('type' , 'upcoming')->orderBy('created_at','DESC')->get(); 
 
-    		return view('home')->with('users', $user)->with('circulars' , $circular)->with('blogs' , $blogs)->with('event',$e);
+    		return view('home')->with('users', $u)->with('circulars' , $circular)->with('blogs' , $blogs)->with('event',$e);
     }
     public function about()
     {
