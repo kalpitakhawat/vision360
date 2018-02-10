2e45b5925db99d8e10f98eed6732d84ff25ab4c4
kalpitakhawat <kalpitakhawat@gmail.com>
2018-02-10 17:50:23 +0530

    Bug Fixes

--- resources/views/admin/addCircular.blade.php
+++ resources/views/admin/addCircular.blade.php
@@ -23,14 +23,13 @@
 										<input type="text" id="title" name="title" class="form-control">
 										<label for="title" class="">Title</label>
 									</div>
-									
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-12">
 									<div class="md-form">
-										<textarea type="text" id="description" name="description" class="md-textarea"></textarea>
-										<label for="description">Description</label>
+										<textarea type="text" id="textarea-description" name="description" class="md-textarea"></textarea>
+										<label for="textarea-description">Description</label>
 									</div>
 								</div>
 							</div>
@@ -41,5 +40,24 @@
 			</div>
 		</main>
 		@include('./includes/scripts')
+		<script src="/js/tinymce/tinymce.min.js"></script>
+		<script type="text/javascript">
+			tinymce.init({
+		selector: '#textarea-description',
+		height: 500,
+		theme: 'modern',
+		plugins: 'print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
+		toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+		image_advtab: true,
+		templates: [
+		{ title: 'Test template 1', content: 'Test 1' },
+		{ title: 'Test template 2', content: 'Test 2' }
+		],
+		content_css: [
+		'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+		'//www.tinymce.com/css/codepen.min.css'
+		]
+		});
+		</script>
 	</body>
 </html>
